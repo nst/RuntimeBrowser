cc92be50bb70712d707ee1a27efe873a8c04ed57
Nicolas Seriot <nicolas@seriot.ch>
2013-12-07 15:22:58 +0100

    use @compatibility_alias directive to avoid twice the same for OS X and iOS

--- UI/NSMutableAttributedString+RTB.h
+++ UI/NSMutableAttributedString+RTB.h
@@ -6,14 +6,17 @@
 //  Copyright (c) 2013 Nicolas Seriot. All rights reserved.
 //
 
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#else
+@compatibility_alias UIColor NSColor;
+@compatibility_alias UIFont NSFont;
+#endif
+
 #import <Foundation/Foundation.h>
 
 @interface NSMutableAttributedString (RTB)
 
-#if TARGET_OS_IPHONE
 - (void)setTextColor:(UIColor *)color font:(UIFont *)font range:(NSRange)range;
-#else
-- (void)setTextColor:(NSColor *)color font:(NSFont *)font range:(NSRange)range;
-#endif
 
 @end
